20f0fef80c92765551c6619a8f678e2441e27d18
Gena <tga001@inbox.ru>
2021-01-23 20:23:36 +0300

    update
    Signed-off-by: Gena <tga001@inbox.ru>

--- src/port.c
+++ src/port.c
@@ -1,16 +1,4 @@
 
-//#include <stdio.h>   /* Standard input/output definitions */
-//#include <string.h>  /* String function definitions */
-//#include <unistd.h>  /* UNIX standard function definitions */
-//#include <fcntl.h>   /* File control definitions */
-//#include <errno.h>   /* Error number definitions */
-//#include <termios.h> /* POSIX terminal control definitions */
-
-//#include <sys/types.h>
-//#include <sys/stat.h>
-//#include <fcntl.h>
-//#include <termios.h>
-//#include <stdio.h>
 
 #include <termios.h>
 #include <stdio.h>
@@ -25,29 +13,18 @@
 #define TRUE  1
 
 // var
-volatile int stop = FALSE;
-
-void signal_handler_IO (int status);	/* объявление обработчика сигнала */
-int wait_flag = TRUE;               	/* TRUE пока не получен сигнал */
-
-int fd;  	/* File descriptor for the port */
-int p1, p2, res;
-//struct termios oldtio,newtio;
-struct sigaction saio;           		/* объявление действия сигнала (signal action) */
-char buf[32768];
 
 /*
  * 'open_port()' - Open serial port 1.
  *
  * Returns the file descriptor on success or -1 on error.
  */
-
 int open_port()
 {
-	//int fd; /* File descriptor for the port */
+	int fd; /* File descriptor for the port */
 
-	fd = open("/dev/ttyUSB0", O_RDWR | O_NOCTTY | O_NDELAY);
-	//fd = open( "/sysroot/dev/ttyUSB0", O_RDWR| O_NOCTTY );
+	//fd = open("/dev/ttyUSB0", O_RDWR | O_NOCTTY | O_NDELAY);
+	fd = open("/dev/ttyACM0", O_RDWR | O_NOCTTY | O_NDELAY);
 	if (fd == -1)
 	{
 		/*
@@ -58,10 +35,6 @@ int open_port()
 	}
 	else
 	{
-		p1  = 0;
-		p2  = 0;
-		res = 0;
-
 		// Flush away any bytes previously read or written.
 		int result = tcflush(fd, TCIOFLUSH);
 		if (result)
@@ -90,7 +63,6 @@ int open_port()
 		options.c_cc[VTIME] = 1;
 		options.c_cc[VMIN]  = 0;
 
-
 		// This code only supports certain standard baud rates. Supporting
 		// non-standard baud rates should be possible but takes more work.
 		cfsetospeed(&options, B115200);
@@ -105,40 +77,17 @@ int open_port()
 		}
 
 		//fcntl(fd, F_SETFL, 0);
-        /*
-          устанавливаем обработчик сигнала перед установкой устройства как асинхронного
-        */
-        //saio.sa_handler = signal_handler_IO;
-        //saio.sa_mask = (__sigset_t)0;
-        //sigemptyset(&saio.sa_mask);
-        //saio.sa_flags = 0;
-        //saio.sa_flags = SA_SIGINFO;
-        //saio.sa_restorer = NULL;
-        //sigaction(SIGIO, &saio, NULL);
-
-        /*
-          разрешаем процессу получать SIGIO
-        */
-        //fcntl(fd, F_SETOWN, getpid());
-
-        /*
-          делаем файловый дескриптор асинхронным (страница руководства
-          говорит, что только O_APPEND и O_NONBLOCK будут работать
-          с F_SETFL...)
-        */
-        //fcntl(fd, F_SETFL, FASYNC);
 
 	}
 
 	return (fd);
 }
 
-void close_port(int fd_l)
+void close_port(int fd)
 {
-	if (fd_l >= 0)
+	if (fd >= 0)
 		{
-			close(fd_l);
-			fd = -1;
+			close(fd);
 		}
 }
 
@@ -180,23 +129,9 @@ ssize_t read_port(int fd, uint8_t * buffer, size_t size)
 	return received;
 }
 
-int set_buffer()
-{
-	buf[p2] = 'q';
-	p2++;
-	buf[p2] = 'w';
-	p2++;
-	buf[p2] = 0;
-	return p2 - p1;
-}
-
-int is_buffer()
+int is_buffer(int fd)
 {
 	int available = 0;
-
-	//if (wait_flag == FALSE) return 1;
-	//else return 0;
-	//return p2 - p1;
 
 	if( ioctl(fd, FIONREAD, &available ) < 0 ) {
 		// Error handling here
@@ -204,42 +139,6 @@ int is_buffer()
 		available = 0;
 	}
 	return available;
-}
-
-void clear_port()
-{
-	p1  = 0;
-	p2  = 0;
-	res = 0;
-
-}
-
-char read_char()
-{
-	char ch = 0;
-
-	//res = read(fd, &buf[p2], 255);
-	//buf[res+p2] = 0;
-	if (p1 < p2) {
-		ch = buf[p1];
-		p1++;
-	}
-	return ch;
-}
-
-/***************************************************************************
-* обработчик сигнала. устанавливает wait_flag в FALSE для индикации        *
-* вышеприведенному циклу, что есть принятый символ                         *
-***************************************************************************/
-void signal_handler_IO (int status)
-{
-	//printf("received SIGIO signal.\n");
-	res = read(fd, &buf[p2], 255);
-	//buf[p2+res] = 0;
-	p2 = p2 + res;
-	buf[p2] = 0;
-
-	wait_flag = FALSE;
 }
 
 
--- src/window.vala
+++ src/window.vala
@@ -19,14 +19,13 @@
 using Gtk;
 //using Posix;
 
+//extern functions from port.c
 extern int  open_port();
 extern void close_port(int fd);
-extern void clear_port();
-extern int set_buffer();
-extern int is_buffer();
-extern char read_char();
+extern int is_buffer(int fd);
 extern ssize_t read_port(int fd, uint8 * buffer, size_t size);
-
+extern int write_port(int fd, uint8 * buffer, size_t size);
+//
 
 namespace ValaTerminal {
 	[GtkTemplate (ui = "/org/example/App/window.ui")]
@@ -42,21 +41,32 @@ namespace ValaTerminal {
         string str;
         int fd;
         int n;
+        int res;
         int t;
         char ch;
         uint8 u8;
-        uint8 buf_u8[2048];
+        uint8 buf_in[2048];
+        uint8 buf_out[256];
         bool  chg;
 
         TextBuffer buffer = new TextBuffer (null); //stores text to be displayed
 
         private void send() {
-            //str = str + edit1.get_text() + "\n\r";
-            str = str + edit1.get_text() + "\n";
-            buffer.set_text(str);
-            //buffer.insert_at_cursor(str+"\n\r", str.length);
-            label1.label = combo1.get_active_text();
-            //label1.label = "Hello World! + " + max.to_string ();
+        	string s;
+        	int len;
+            s = edit1.get_text() + "\n\r";
+            len = s.length;
+            //len = 6;
+            for (int i=0;i<len;i++)
+            	buf_out[i] = (uint8)s[i];
+            //buf_out[0] = (uint8)'h';
+            //buf_out[1] = (uint8)'e';
+            //buf_out[2] = (uint8)'l';
+            //buf_out[3] = (uint8)'p';
+            //buf_out[4] = (uint8)'\n';
+            //buf_out[5] = (uint8)'\r';
+            //buf_out[6] = 0;
+			write_port(fd, buf_out, (size_t)len);
         }
 
         private void port_ctrl() {
@@ -88,8 +98,6 @@ namespace ValaTerminal {
 
             str = "";
             fd  = -1;
-            clear_port();
-            n = set_buffer();
             t = 0;
             ch = ' ';
             chg = false;
@@ -103,42 +111,25 @@ namespace ValaTerminal {
 
             TimeoutSource time = new TimeoutSource(100);   // set timer in millisecond
             time.set_callback(() => {
-                //t++;
-                //label1.label = "Hello World! + " + t.to_string ();
-                //while ((fd >= 0) && (is_buffer() > 0)) {
-                //	read_char();
-                //	str = str + "Hello"; //(string)read_char();
-                //}
-				//n = is_buffer();
-				//n = (int)read_port(fd, buf_u8, 1);
-				//label1.label = "Cnt n = " + n.to_string ();
-				//str = "Cnt n = " + n.to_string ();
-				//if (n > 0) {
-				//	ch = read_char();
-				//	str = str + " ch = " + ch.to_string();
-				//}
-				n = 0;
-				if ((fd >= 0) && (is_buffer() > 0)) {
-					n = is_buffer();
+				//t = 0;
+				if (fd >= 0) {
+					n = is_buffer(fd);
 					label1.label = "Cnt n = " + n.to_string ();
-					n = (int)read_port(fd, buf_u8, 1);
+					res = (int)read_port(fd, buf_in, n);
+					t = 0;
 				}
-				while ((fd >= 0) && (n > 0)) {
-					u8 = buf_u8[0];
+				while ((fd >= 0) && (n > t)) {
+					u8 = buf_in[t];
 					ch = (char)u8;
 					if (ch != '\r')
-						str = str + "" + ch.to_string();
-					n = 0;
-					if (is_buffer() > 0)
-						n = (int)read_port(fd, buf_u8, 1);
-					//t++;
+						str = str + ch.to_string();
+					t++;
 					chg = true;
 				}
 				//t = 0;
 				if (chg)
                 	buffer.set_text(str);
                 chg = false;
-                //label1.label = str;
 
                 return true;    // timer continue
                 //return false;   // timer stop
